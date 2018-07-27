c70ae1414d5da57d1651585e28cded687d175ef5
breadthe <ferdinand.bismarck@gmail.com>
2018-07-27 18:12:37 -0500

    Style the logo

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand d-flex" href="{{ url('/') }}">
             <i class="fa fa-film fa-2x" aria-hidden="true"></i>&nbsp;
-            {{ config('app.name', 'MovieBuff') }}
+            <span>{{ config('app.name', 'MovieBuff') }}</span>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
